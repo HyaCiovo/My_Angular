85e0c2fbd68727e423511bc967757c7df466334c
zhujiruo <zhujiruo@foxmail.com>
2023-06-14 17:35:12 +0800

    ✨ feat:优化主题色选择

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+enum themeEnum {
+  "infinity-theme" = "无限",
+  "provence-theme" = "普罗旺斯",
+  "sweet-theme" = "蜜糖",
+  "deep-theme" = "深邃夜空",
+}
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
   themeOptions = [
-    { name: '无限', value: 'infinity-theme' },
-    { name: '普罗旺斯', value: 'provence-theme' },
-    { name: '蜜糖', value: 'sweet-theme' },
-    { name: '深邃夜空', value: 'deep-theme' }]
-  defaultValue = this.themeOptions[0]
+    { name: "无限", value: "infinity-theme" },
+    { name: "普罗旺斯", value: "provence-theme" },
+    { name: "蜜糖", value: "sweet-theme" },
+    { name: "深邃夜空", value: "deep-theme" },
+  ];
+  defaultValue = themeEnum[window["devuiThemeService"].currentTheme.id];
   changeTheme(theme: any) {
-    (<any>window)["devuiThemeService"].applyTheme((<any>window)["devuiThemes"][theme.value]);
+    window["devuiThemeService"].applyTheme(
+      (<any>window)["devuiThemes"][theme.value]
+    );
   }
   initTheme() {
-    (<any>window)["devuiThemeService"].applyTheme((<any>window)["devuiThemes"][this.themeOptions[0].value])
+    window["devuiThemeService"].applyTheme(
+      window["devuiThemes"][this.themeOptions[0].value]
+    );
   }
   ngOnInit(): void {
-    this.initTheme()
+    // this.initTheme()
+    // this.defaultValue == (<any>window)["devuiThemeService"].currentTheme.id;
   }
 }
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
 export class AboutComponent {
+  showLoading: boolean = false;
+  async toggleLoading() {
+    this.showLoading = true;
+    await this.fetchData();
+    this.showLoading = false;
+  }
   fetchData() {
-    console.log("fetching")
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 1500);
+    });
   }
 }
